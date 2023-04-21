8306b1f4a580d2fc64cb3f11f0300fdda960a5cd
Gabe <soccernerdgj@gmail.com>
2023-04-21 09:06:44 -0600

    cleaning up

--- stp_resources/admin.py
+++ stp_resources/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from .models import Post, WebResource, Question, Comment
+from .models import Post, WebResource, Question
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(WebResource)
 admin.site.register(Question)
-from .models import Post, Comment
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'post', 'created', 'active')
-    list_filter = ('active', 'created', 'updated')
-    search_fields = ('name', 'email', 'body')
+from .models import Post
